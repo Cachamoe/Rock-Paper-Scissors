1b22f211ea95655b3c98a578fc241cefd31cf441
Caleb Chaney <calebc347@gmail.com>
2021-10-03 08:45:58 -0500

    Updated game.py

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-import random
+import random, os, sys
 userScore = 0 
 computerScore = 0
 userInput = ""
 computerInput = ""
 choice = ""
+numGames = int(input("How many games would you like to play?\n"))
 userWin = False
 draw = False
+clear = lambda: os.system("cls")
