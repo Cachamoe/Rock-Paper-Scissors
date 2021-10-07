95aa3ba21f9b0bbe364d309c94c28d332b26e1d4
Caleb Chaney <calebc347@gmail.com>
2021-10-07 17:00:09 -0500

    Updated game.py

--- game.py
+++ game.py
@@ -8,3 +8,21 @@ numGames = int(input("How many games would you like to play?\n"))
 userWin = False
 draw = False
 clear = lambda: os.system("cls")
+
+
+userInput = input("Choose rock, paper, or sissors\n")
+
+
+
+
+
+
+
+for x in range(0, numGames):
+    generatedNum = random.randint(1,3)
+    if generatedNum == 1:
+        computerInput = "rock"
+    elif generatedNum == 2:
+        computerInput = "paper"
+    elif generatedNum == 3:
+        computerInput = "sissors"
